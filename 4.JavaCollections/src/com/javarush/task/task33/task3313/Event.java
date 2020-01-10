aed4888c906658092def4d9e9f297e107e234bcc
DmSudin <dm.sudin@gmail.com>
2020-01-10 23:44:03 +0300

    task 3313

--- 4.JavaCollections/src/com/javarush/task/task33/task3313/Event.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3313/Event.java
@@ -6,6 +6,10 @@ import java.util.Date;
 
 public class Event {
     public String name;
+@JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "dd-MM-yyyy hh:mm:ss"
+)
 
     public Date eventDate;
 
