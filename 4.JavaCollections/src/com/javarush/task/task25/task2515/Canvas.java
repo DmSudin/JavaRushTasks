d383588a733b95828d5e6a3f2ed5ba51c195f2b4
DmSudin <dm.sudin@gmail.com>
2020-02-28 22:28:45 +0300

    Space9

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -38,4 +38,23 @@ public class Canvas {
         }
     }
 
+    public void clear() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                setPoint(i, j, ' ');
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
+
 }
