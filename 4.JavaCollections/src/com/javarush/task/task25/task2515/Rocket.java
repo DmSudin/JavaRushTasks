c8173f2b8127222b07324aecdd82cf842e5514f2
DmSudin <dm.sudin@gmail.com>
2020-02-28 22:46:27 +0300

    Space11

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,7 +1,19 @@
 package com.javarush.task.task25.task2515;
 
 public class Rocket extends BaseObject {
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.setPoint(x, y, 'R');
+    }
+
+    @Override
+    public void move() {
+        super.move();
+        y--;
     }
 }
