3bec2c02d4d60f8587e1fc49a41fb085d8bd0589
DmSudin <dm.sudin@gmail.com>
2020-02-29 23:01:26 +0300

    Space15

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -1,37 +1,53 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для НЛО
+ */
 public class Ufo extends BaseObject {
+    //картинка для отрисовки
     private static int[][] matrix = {
             {0, 0, 0, 0, 0},
             {0, 0, 1, 0, 0},
-            {0, 0, 1, 0, 0},
-            {1, 0, 1, 0, 1},
             {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
     };
+
     public Ufo(double x, double y) {
         super(x, y, 3);
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
-        super.draw(canvas);
-        canvas.drawMatrix(x-radius+1, y-radius+1, matrix, 'U');
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
     }
 
+    /**
+     * Двигаем себя на один ход в случайном направлении.
+     */
     @Override
     public void move() {
-        super.move();
-        if (Math.random() >= 0.9) fire();
-        double dx = Math.random()*2-1;
-        double dy = Math.random()*2-1;
-        x+=dx;
-        y+=dy;
-        checkBorders(0, Space.game.getWidth(), y, Space.game.getHeight()/2);
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
 
+        x += dx;
+        y += dy;
 
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
+
+        int random10 = (int) (Math.random() * 10);
+        if (random10 == 0)
+            fire();
     }
 
+    /**
+     * Стреляем.
+     * Сбрасываем(создаем) одну бомбу прямо под собой.
+     */
     public void fire() {
-        Space.game.getBombs().add(new Bomb(x, y + radius));
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
