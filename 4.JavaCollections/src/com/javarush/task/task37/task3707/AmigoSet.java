150683c4f6a19c4e6de4e2b9d34825fe0856bb93
DmSudin <dm.sudin@gmail.com>
2020-04-07 23:57:31 +0300

    AmigoSet5

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task37.task3707;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -92,4 +92,48 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
     public boolean contains(Object o) {
         return map.containsKey(o);
     }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        int capacity = HashMapReflectionHelper.callHiddenMethod(map, "capacity");
+        float loadFactor = HashMapReflectionHelper.callHiddenMethod(map, "loadFactor");
+        out.writeInt(capacity);
+        out.writeInt(map.size());
+        out.writeFloat(loadFactor);
+        for (E e : map.keySet()) {
+            out.writeObject(e);
+        }
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        int capacity = in.readInt();
+        int size = in.readInt();
+        float loadFactor = in.readFloat();
+        this.map = new HashMap<>(capacity, loadFactor);
+        for (int i = 0; i < size; i++) {
+            E e = (E) in.readObject();
+            map.put(e, PRESENT);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        AmigoSet initialAmigoSet = new AmigoSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            initialAmigoSet.add(i);
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(initialAmigoSet);
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(byteArrayOutputStream.toByteArray()));
+        AmigoSet loadedAmigoSet = (AmigoSet) objectInputStream.readObject();
+
+        System.out.println(initialAmigoSet.size() + " " + loadedAmigoSet.size());
+    }
+
+
 }
