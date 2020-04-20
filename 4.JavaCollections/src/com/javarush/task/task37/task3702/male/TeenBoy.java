7bb5c9b25d8ffbb8bb38f639d4b86a3e56a9bd4f
DmSudin <dm.sudin@gmail.com>
2020-04-20 22:37:36 +0300

    Factory1

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/TeenBoy.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class TeenBoy implements Human{
+        public static final int MAX_AGE = 19;
+
+        public String toString() {
+            return "TeenBoy{}";
+        }
+}
